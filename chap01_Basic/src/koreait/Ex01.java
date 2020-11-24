1ccbe28c179ae63c6e19a6ac1e32b8d46a783b6a
Heo0916 <gigi916@naver.com>
2020-11-24 20:32:35 +0900

    java 공부 1일차

---
+++ chap01_Basic/src/koreait/Ex01.java
@@ -0,0 +1,30 @@
+package koreait;
+
+//한 줄 주석 : 패키지 - 유사한 클래스들의 모임(폴더와 유사)
+
+/*
+ * 두 주 이상 주석
+ * 작성자 : 허 동 기
+ * 작성일 : 2018.07.09
+ * 작성내용 : java 기초
+ */
+public class Ex01 { // Ex01 클래스 시작
+
+	public static void main(String[] args) { //main 메서드 시작
+		
+		System.out.println("자바 프로그램에 오신 걸 환영합니다.");
+		System.out.println("안녕하세요~~~");
+		System.out.println("반갑습니다.~~~");
+		/*
+		 * System.out : 표준출력장치(모니터, 콘솔)
+		 * println() : 지정된 장치에 결과를 출력하는 메서드
+		 *  =>ln : line(출력 한 후 출바꿈)
+		 *  print() : 다음 줄로 이동하지 않고 옆(오른쪽)에 출력.
+		 *  
+		 *  단축키
+		 *  syso + ctrl + space bar ==> 자동 완성 기능
+		 *  ctrl + F11 ==> 실행 단축 키
+		 */
+		
+	} // main 메서드 end
+} //Ex01 클래스 end
---
+++ chap01_Basic/src/koreait/Ex02.java
@@ -0,0 +1,18 @@
+package koreait;
+
+/*
+ * [문제] 아래와 같이 콘솔 창에 출력하세요.
+ *       이 름 : 허 동 기
+ *       연락처 : 010-8793-9619
+ *       이메일 : gigi916@naver.com
+ *       주 소 : 경기도 부천시 원미구
+ */
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
